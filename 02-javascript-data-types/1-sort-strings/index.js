fce0ebfa676277d1cd3b907901871d4077abdcac
Spark345 <90181596+Spark345@users.noreply.github.com>
2022-09-19 11:31:29 +0300

    module 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,18 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+
+
+export function sortStrings(arr, param= 'asc') {
+  const arrCopy = [...arr];
+  if(param === 'asc'){
+   arrCopy.sort((a, b) => a.localeCompare(b, ['ru', 'en'], {sensitivity: 'variant', caseFirst: 'upper' }))
+  }
+  if(arr === arr && param === 'desc'){
+      arrCopy.sort((a, b) => -a.localeCompare(b, ['ru', 'en'], {sensitivity: 'variant', caseFirst: 'upper' }))
+    }
+
+  return arrCopy;
 
 }
+
