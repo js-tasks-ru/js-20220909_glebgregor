086644e27d5a92ae2b53833320db8e82281726c8
Spark345 <90181596+Spark345@users.noreply.github.com>
2022-10-05 17:19:08 +0300

    module 5 and 6

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,68 @@
 class Tooltip {
-  initialize () {
+  static instance;
+
+  element;
+
+
+  pointerOver = event =>{
+    const element = event.target.closest(`[data-tooltip]`)
+
+    if(element){
+      this.render(element.dataset.tooltip)
+      document.addEventListener("pointermove", this.onPointerMove)
+    }
 
   }
+  pointerOut = () =>{
+    this.remove()
+    document.removeEventListener("pointermove", this.onPointerMove)
+  }
+  onPointerMove = event => {
+    const left = event.clientX + 10;
+    const top = event.clientY + 10;
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${left}px`;
+  }
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  initialize () {
+    this.initEventListeners();
+  }
+  initEventListeners(){
+    document.addEventListener("pointerover", this.pointerOver);
+    document.addEventListener("pointerout", this.pointerOut)
+  }
+
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
+
+  remove(){
+    if(this.element){
+      this.element.remove()
+    }
+
+  }
+  destroy(){
+    this.remove();
+    this.element = null;
+    this.subElements = {}
+  }
+
+
 }
 
 export default Tooltip;
